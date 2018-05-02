249beeb52f2f9ce12f016ac329f8ca70e5b847e2
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2018-05-01 22:07:53 -0700

    Create nextmd_favicon.user.js

---
+++ nextmd_favicon.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name           NextMD Favicon
+// @description    Adds a favicon to NextMD
+// @author         Buk Lau
+// @version        0.1
+// @include        https://www.nextmd.com/ud2/*
+// @grant          none
+// ==/UserScript==
+
+var favicon_link_html = document.createElement('link');
+favicon_link_html.rel = 'icon';
+favicon_link_html.href = 'https://github.com/MrBukLau/Favicons-for-Websites/blob/master/NextMD%20Favicon.png?raw=true';
+favicon_link_html.type = 'image/png';
+
+try {
+  document.getElementsByTagName('head')[0].appendChild( favicon_link_html );
+}
+catch(e) { }
