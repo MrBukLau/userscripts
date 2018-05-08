a78655ebb0f2901e06427bc7c1c5d719365eb781
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2018-05-07 17:26:50 -0700

    Create myhealthconnection_favicon.user.js

---
+++ myhealthconnection_favicon.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name           MyHealthConnection Favicon
+// @description    Adds a favicon to MyHealthConnection
+// @author         Buk Lau
+// @version        0.1
+// @include        *secure.orclinic.com*
+// @grant          none
+// ==/UserScript==
+
+var favicon_link_html = document.createElement('link');
+favicon_link_html.rel = 'icon';
+favicon_link_html.href = 'http://www.oregonclinic.com/sites/default/files/favicon_32px.ico';
+favicon_link_html.type = 'image/ico';
+
+try {
+  document.getElementsByTagName('head')[0].appendChild( favicon_link_html );
+}
+catch(e) { }
