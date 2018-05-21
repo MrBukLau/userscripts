10adbf2b190b36cb11584eff4bed7612be3f2bd2
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2018-05-21 14:22:52 -0700

    Update bring_back_old_reddit.user.js

--- bring_back_old_reddit.user.js
+++ bring_back_old_reddit.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name               Bring Back Old Reddit
-// @namespace          https://greasyfork.org/en/users/105361-randomusername404
+// @version            1.0
 // @description        Always redirects to old-Reddit to avoid Reddit's redesign
 // @icon               https://www.redditstatic.com/desktop2x/img/favicon/apple-icon-76x76.png
 // @include            *://www.reddit.com/*
-// @version            1.0
-// @run-at             document-start
 // @author             RandomUsername404
 // @modifier           Buk Lau
 // @grant              none
+// @run-at             document-start
 // ==/UserScript==
 
 window.location.replace("https://old.reddit.com" + window.location.pathname);
