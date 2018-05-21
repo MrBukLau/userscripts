fcc15499d13a3d05fb652705084abbfa0320159f
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2018-05-21 14:30:45 -0700

    Update stop_reddit_from_tracking.user.js

--- stop_reddit_from_tracking.user.js
+++ stop_reddit_from_tracking.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         Stop Reddit from Tracking
-// @description  Replaces data-outbound-url with data-href-url
-// @icon         https://www.redditstatic.com/desktop2x/img/favicon/apple-icon-76x76.png
-// @homepage     https://old.reddit.com/r/privacy/comments/4aqdg0/reddit_started_tracking_the_links_we_click_heres/
-// @namespace    http://reddit.com/u/OperaSona
-// @author       OperaSona
-// @version      1.0
-// @match        *://*.reddit.com/*
-// @grant        none
+// @name               Stop Reddit from Tracking
+// @version            1.0
+// @description        Replaces data-outbound-url with data-href-url
+// @icon               https://www.redditstatic.com/desktop2x/img/favicon/apple-icon-76x76.png
+// @match              *://*.reddit.com/*
+// @homepage           https://old.reddit.com/r/privacy/comments/4aqdg0/reddit_started_tracking_the_links_we_click_heres/
+// @author             OperaSona
+// @modifier           Buk Lau
+// @grant              none
 // ==/UserScript==
 
 var a_col = document.getElementsByTagName('a');
