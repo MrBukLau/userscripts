3a648205c9b68f9f7fe6e82b39b96d3bf965c91a
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2018-05-21 14:52:51 -0700

    Update bring_back_old_reddit.user.js

--- bring_back_old_reddit.user.js
+++ bring_back_old_reddit.user.js
@@ -4,6 +4,7 @@
 // @description        Always redirects to old-Reddit to avoid Reddit's redesign
 // @icon               https://www.redditstatic.com/desktop2x/img/favicon/apple-icon-76x76.png
 // @include            *://www.reddit.com/*
+// @homepage           https://greasyfork.org/en/scripts/44669-bring-back-old-reddit
 // @author             RandomUsername404
 // @modifier           Buk Lau
 // @grant              none
