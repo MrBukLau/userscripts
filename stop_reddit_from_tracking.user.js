125098ffe9b5ee590d2bd3bf418d18f3171daf7c
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2018-05-21 13:58:02 -0700

    Create stop_reddit_from_tracking.user.js

---
+++ stop_reddit_from_tracking.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Stop Reddit from Tracking
+// @description  Replaces data-outbound-url with data-href-url
+// @icon         https://www.redditstatic.com/desktop2x/img/favicon/apple-icon-76x76.png
+// @author       OperaSona
+// @match        *://*.reddit.com/*
+// @grant        none
+// ==/UserScript==
+
+var a_col = document.getElementsByTagName('a');
+var a, actual_fucking_url;
+for(var i = 0; i < a_col.length; i++) {
+  a = a_col[i];
+  actual_fucking_url = a.getAttribute('data-href-url');
+  if(actual_fucking_url) a.setAttribute('data-outbound-url', actual_fucking_url);
+}
