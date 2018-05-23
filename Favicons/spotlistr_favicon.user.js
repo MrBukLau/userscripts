3f26d62d643a218da2aba09f362cd089a56ce7ea
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2018-05-22 17:39:08 -0700

    Update spotlistr_favicon.user.js

--- Favicons/spotlistr_favicon.user.js
+++ Favicons/spotlistr_favicon.user.js
@@ -2,7 +2,7 @@
 // @name               Spotlistr Favicon
 // @version            0.1
 // @description        Adds a favicon to Spotlistr
-// @icon               https://github.com/MrBukLau/Tampermonkey-Scripts/blob/master/Favicons/Spotlistr%20Favicon.png?raw=true
+// @icon               https://raw.githubusercontent.com/MrBukLau/Tampermonkey-Userscripts/master/Favicons/Images/Spotlistr%20Favicon.png
 // @include            *spotlistr.herokuapp.com/*
 // @author             Buk Lau
 // @grant              none
@@ -10,7 +10,7 @@
 
 var favicon_link_html = document.createElement('link');
 favicon_link_html.rel = 'icon';
-favicon_link_html.href = 'https://github.com/MrBukLau/Tampermonkey-Scripts/blob/master/Favicons/Spotlistr%20Favicon.png?raw=true';
+favicon_link_html.href = 'https://raw.githubusercontent.com/MrBukLau/Tampermonkey-Userscripts/master/Favicons/Images/Spotlistr%20Favicon.png';
 favicon_link_html.type = 'image/png';
 
 try {
