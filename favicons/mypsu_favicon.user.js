890dc747f981fdf76170e267f7416916feef35cd
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2018-07-07 20:27:54 -0700

    Update mypsu_favicon.user.js

--- favicons/mypsu_favicon.user.js
+++ favicons/mypsu_favicon.user.js
@@ -2,7 +2,7 @@
 // @name               myPSU - Favicon
 // @version            0.1
 // @description        Adds a favicon to myPSU
-// @icon               https://www.pdx.edu/sites/all/themes/pdx_respond/favicon.ico
+// @icon               https://www.google.com/s2/favicons?domain=pdx.edu
 // @include            *my.pdx.edu*
 // @author             Buk Lau
 // @grant              none
@@ -10,8 +10,8 @@
 
 var favicon_link_html = document.createElement('link');
 favicon_link_html.rel = 'icon';
-favicon_link_html.href = 'https://www.pdx.edu/sites/all/themes/pdx_respond/favicon.ico';
-favicon_link_html.type = 'image/ico';
+favicon_link_html.href = 'https://www.google.com/s2/favicons?domain=pdx.edu';
+favicon_link_html.type = 'image/png';
 
 try {
   document.getElementsByTagName('head')[0].appendChild( favicon_link_html );
