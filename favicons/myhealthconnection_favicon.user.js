d327463faea3ed04ab9db19f637193c81baedf2b
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2018-07-07 20:27:24 -0700

    Update myhealthconnection_favicon.user.js

--- favicons/myhealthconnection_favicon.user.js
+++ favicons/myhealthconnection_favicon.user.js
@@ -2,7 +2,7 @@
 // @name               MyHealthConnection - Favicon
 // @version            0.1
 // @description        Adds a favicon to MyHealthConnection
-// @icon               https://raw.githubusercontent.com/MrBukLau/tampermonkey-userscripts/master/favicons/images/MyHealthConnection%20Favicon.ico
+// @icon               https://www.google.com/s2/favicons?domain=orclinic.com
 // @include            *secure.orclinic.com*
 // @author             Buk Lau
 // @grant              none
@@ -10,8 +10,8 @@
 
 var favicon_link_html = document.createElement('link');
 favicon_link_html.rel = 'icon';
-favicon_link_html.href = 'https://raw.githubusercontent.com/MrBukLau/tampermonkey-userscripts/master/favicons/images/MyHealthConnection%20Favicon.ico';
-favicon_link_html.type = 'image/ico';
+favicon_link_html.href = 'https://www.google.com/s2/favicons?domain=orclinic.com';
+favicon_link_html.type = 'image/png';
 
 try {
   document.getElementsByTagName('head')[0].appendChild( favicon_link_html );
