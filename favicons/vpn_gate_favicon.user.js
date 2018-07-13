d3d312a665549ae93ec65e0343d9d28698b4a847
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2018-07-13 16:56:26 +0800

    Update vpn_gate_favicon.user.js

--- favicons/vpn_gate_favicon.user.js
+++ favicons/vpn_gate_favicon.user.js
@@ -2,7 +2,7 @@
 // @name               VPN Gate - Favicon
 // @version            0.1
 // @description        Adds a favicon to VPN Gate
-// @icon               https://www.google.com/s2/favicons?domain=openvpn.net
+// @icon               https://pbs.twimg.com/profile_images/3388522138/269e993c7d277a7d6a5ba9378b4559bc_400x400.png
 // @include            *vpngate.net*
 // @author             Buk Lau
 // @grant              none
@@ -10,7 +10,7 @@
 
 var favicon_link_html = document.createElement('link');
 favicon_link_html.rel = 'icon';
-favicon_link_html.href = 'https://www.google.com/s2/favicons?domain=openvpn.net';
+favicon_link_html.href = 'https://pbs.twimg.com/profile_images/3388522138/269e993c7d277a7d6a5ba9378b4559bc_400x400.png';
 favicon_link_html.type = 'image/png';
 
 try {
