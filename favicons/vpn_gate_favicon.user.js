efa4ccd13010abb5b9a8de4f564467caa7d1805b
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2018-07-13 16:51:20 +0800

    Create vpn_gate_favicon.user.js

---
+++ favicons/vpn_gate_favicon.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name               VPN Gate - Favicon
+// @version            0.1
+// @description        Adds a favicon to VPN Gate
+// @icon               https://www.google.com/s2/favicons?domain=vpngate.net
+// @include            *vpngate.net*
+// @author             Buk Lau
+// @grant              none
+// ==/UserScript==
+
+var favicon_link_html = document.createElement('link');
+favicon_link_html.rel = 'icon';
+favicon_link_html.href = 'https://www.google.com/s2/favicons?domain=vpngate.net';
+favicon_link_html.type = 'image/png';
+
+try {
+  document.getElementsByTagName('head')[0].appendChild( favicon_link_html );
+}
+catch(e) { }
