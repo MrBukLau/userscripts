b352824b515fd53eaeee7aa9dba7dc9a46d987a0
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2019-04-28 15:51:46 -0700

    Update youtube_always_theater_mode.user.js

--- javascripts/youtube_always_theater_mode.user.js
+++ javascripts/youtube_always_theater_mode.user.js
@@ -1,29 +1,21 @@
 // ==UserScript==
 // @name               YouTube - Always Theater Mode
-// @version            1.0
+// @version            1.4.1
 // @description        Set the default viewing mode to Theater Mode.
 // @icon               https://www.google.com/s2/favicons?domain=www.youtube.com
 // @include            https://www.youtube.com/*
-// @homepage           https://greasyfork.org/en/scripts/368003-youtube-always-theater-mode
-// @author             Volkan K.
+// @homepage           https://greasyfork.org/en/scripts/10523-youtube-always-theater-mode
+// @author             r-a-y
 // @modifier           Buk Lau
 // @license            GPL v3
 // @grant              none
 // @run-at             document-start
-// @require 	       https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js
-// @require            https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.0.4/js.cookie.min.js
 // ==/UserScript==
 
-this.$ = this.jQuery = jQuery.noConflict(true);
-
-function set_wide_on() {
-	if (Cookies.get('wide')!=1){
-		Cookies.set('wide', 1, { domain: '.youtube.com' , expires: 365 });
-		var newPlayer = document.querySelector('button.ytp-size-button');
-		if ( newPlayer ) {
-			newPlayer.click();
-		}
+// Toggle Theater Mode after YouTube finishes loading a video.
+window.addEventListener("yt-navigate-finish", function(event) {
+	var newPlayer = document.querySelector('button.ytp-size-button');
+	if ( newPlayer && null === document.getElementById('player-theater-container').firstChild ) {
+		newPlayer.click();
 	}
-}
-
-setInterval(set_wide_on, 1000);
+});
