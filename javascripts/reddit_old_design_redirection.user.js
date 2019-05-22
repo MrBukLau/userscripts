47fbd2b8f9ffeecba7a0aca08792cb60b5841c6b
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2019-05-21 21:29:05 -0700

    Update reddit_old_design_redirection.user.js

--- javascripts/reddit_old_design_redirection.user.js
+++ javascripts/reddit_old_design_redirection.user.js
@@ -2,7 +2,7 @@
 // @name               Reddit - Old Design Redirection
 // @version            1.0
 // @description        Always redirects to old-Reddit to avoid Reddit's redesign
-// @icon               https://www.google.com/s2/favicons?domain=www.reddit.com
+// @icon               https://www.reddit.com/favicon.ico
 // @include            *://www.reddit.com/*
 // @homepage           https://greasyfork.org/en/scripts/44669-bring-back-old-reddit
 // @author             RandomUsername404
