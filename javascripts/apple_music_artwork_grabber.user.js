bed9efa5c3a7bc0085054fdd70e8e51e50277414
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2020-04-16 22:25:43 -0700

    Update apple_music_artwork_grabber.user.js

--- javascripts/apple_music_artwork_grabber.user.js
+++ javascripts/apple_music_artwork_grabber.user.js
@@ -3,7 +3,7 @@
 // @version            1.0
 // @description        Changes the resolution and format of an artwork image
 // @icon               https://music.apple.com/assets/favicon/favicon-180-eb957907435b0f6386bf2367d57d75f6.png
-// @include            *://is5-ssl.mzstatic.com/image/thumb/Music*
+// @include            *mzstatic.com/image/thumb/Music*
 // @homepage           https://sleazyfork.org/en/forum/discussion/9158/replace-string-in-an-url
 // @author             Vivre
 // @modifier           MrBukLau
