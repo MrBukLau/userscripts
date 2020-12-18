311e238cdf7fb70f506b739ad0930a804593e9c1
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2020-12-18 15:16:51 -0800

    Create kwik_watermark_removal.user.js

---
+++ javascripts/kwik_watermark_removal.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name               Kwik - Watermark Removal
+// @version            1.0
+// @description        Hides the Kwik watermark from videos hosted on Kwik (Credit to SakamotoDesu)
+// @icon               https://kwik.cx/favicon.ico
+// @include            *://kwik.cx/*
+// @homepage           https://old.reddit.com/r/animepiracy/comments/kfdmgg/any_way_to_block_the_kwik_watermark_on_animepahe/gg93qll/
+// @author             MrBukLau
+// @grant              none
+// @run-at             document-start
+// ==/UserScript==
+
+(function() {
+    var css = document.createElement("style");
+        css.setAttribute('type', 'text/css');
+        css.innerText = '.plyr.plyr--paused::after,.plyr.plyr--playing::after{opacity:0 !important}';
+        document.head.appendChild(css);
+})();
