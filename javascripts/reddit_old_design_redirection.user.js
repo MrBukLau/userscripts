576d632fb1afb115db53898759c3310422f5467f
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-04-27 22:54:56 -0700

    Update reddit_old_design_redirection.user.js

--- javascripts/reddit_old_design_redirection.user.js
+++ javascripts/reddit_old_design_redirection.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name               Reddit - Old Design Redirection
 // @version            1.0
-// @description        Redirects to "Old Reddit" to avoid Reddit's redesign (Credit to RandomUsername404)
+// @description        Redirect to "Old Reddit" to avoid Reddit's redesign (Credit to RandomUsername404)
 // @icon               https://www.reddit.com/favicon.ico
 // @include            *://www.reddit.com/*
 // @homepage           https://greasyfork.org/en/scripts/44669-bring-back-old-reddit
