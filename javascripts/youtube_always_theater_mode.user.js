1174ec3f49bf0ad2c7ecd9f4c0f82d3320b0b33b
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-04-27 22:17:08 -0700

    Update youtube_always_theater_mode.user.js

--- javascripts/youtube_always_theater_mode.user.js
+++ javascripts/youtube_always_theater_mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name               YouTube - Always Theater Mode
 // @version            1.4.1
-// @description        Set the default viewing mode to Theater Mode (Credit to r-a-y)
+// @description        Sets the default viewing mode to Theater Mode (Credit to r-a-y)
 // @icon               https://s.ytimg.com/yts/img/favicon_144-vfliLAfaB.png
 // @include            https://www.youtube.com/*
 // @homepage           https://greasyfork.org/en/scripts/10523-youtube-always-theater-mode
