2502e9ce62e439f41eedd1c75493044304ee5e33
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-12-06 10:24:13 -0800

    Update reddit_ios_pop_up_removal.user.js

--- javascripts/reddit_ios_pop_up_removal.user.js
+++ javascripts/reddit_ios_pop_up_removal.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name               Reddit - iOS Pop-up Removal
 // @version            1.0
-// @description        Remove the pop-ups and banners from the iOS version of Reddit on Safari (Special thanks to StephenP for creating the original userscript)
+// @description        Remove the pop-ups and banners from the iOS version of Reddit on Safari
 // @icon               https://www.reddit.com/favicon.ico
 // @match              https://www.reddit.com/*
-// @homepage           https://greasyfork.org/en/scripts/395497-login-reminder-popup-remover
+// @homepage           https://github.com/MrBukLau/userscripts-for-google-chrome
 // @author             MrBukLau
 // @grant              none
 // @run-at             document-start
