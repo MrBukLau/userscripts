6c83c2cc10f189c81d8ac6e2b97cdeb708d35936
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-12-05 22:45:55 -0800

    Update apple_music_artwork_changer.user.js

--- javascripts/apple_music_artwork_changer.user.js
+++ javascripts/apple_music_artwork_changer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name               Apple Music - Artwork Changer
 // @version            1.0
-// @description        Change the image resolution and format for an artwork on Apple Music (Special thanks to Vivre for the coding tip)
+// @description        Change the image resolution and format for an artwork on Apple Music (Special thanks to Vivre for providing some coding tips)
 // @icon               https://music.apple.com/assets/favicon/favicon-180-c132a95549a91ae6983a4914da3e1c44.png
 // @include            *mzstatic.com/image/thumb*
 // @homepage           https://sleazyfork.org/en/forum/discussion/9158/replace-string-in-an-url
