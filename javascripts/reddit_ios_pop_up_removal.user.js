3f0242682e5dc31abb4677a38599d85d2dff9593
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-12-05 22:25:20 -0800

    Update reddit_ios_pop_up_removal.user.js

--- javascripts/reddit_ios_pop_up_removal.user.js
+++ javascripts/reddit_ios_pop_up_removal.user.js
@@ -9,3 +9,10 @@
 // @grant              none
 // @run-at             document-start
 // ==/UserScript==
+
+let st = document.createElement("STYLE");
+if (document.location.href.includes("reddit.com")) {
+    st.textContent = ".TopNav__promoButton, .XPromoAppStoreFooter, .XPromoBlockingModal, .XPromoChoiceBanner, .XPromoInFeed, .XPromoPill, .XPromoPopup, shreddit-experience-tree, [data-testid=bottom-cell-wrapper]{display: none !important} .NavFrame {overflow-y: scroll !important} .scroll-disabled {overflow-y: scroll !important; position: static !important} .scroll-is-blocked {overflow-y: scroll !important}";
+}
+document.getElementsByTagName("HEAD")[0].appendChild(st);
+}
