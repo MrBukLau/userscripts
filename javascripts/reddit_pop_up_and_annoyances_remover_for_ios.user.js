56d55a6421d7ff83699ec95dddd0be27f2338dae
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-12-09 10:20:25 -0800

    Update reddit_pop_up_and_annoyances_remover_for_ios.user.js

--- javascripts/reddit_pop_up_and_annoyances_remover_for_ios.user.js
+++ javascripts/reddit_pop_up_and_annoyances_remover_for_ios.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name               Reddit - Pop-up and Annoyances Remover for iOS
 // @version            1.0
-// @description        Remove pop-ups, banners, and other annoyances from the iOS version of Reddit on Safari
+// @description        Remove pop-ups, banners, and other annoyances from Reddit on Safari for iOS
 // @icon               https://www.reddit.com/favicon.ico
 // @match              https://www.reddit.com/*
 // @homepage           https://github.com/MrBukLau/userscripts
