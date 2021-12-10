67085fbb19091e4332bbb3ab0fb732c6598128c3
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-12-09 23:07:09 -0800

    Update and rename apple-music-artwork-changer.user.js to apple-music-album-artwork-resizer.user.js

--- javascripts/apple-music-album-artwork-resizer.user.js
+++ javascripts/apple-music-album-artwork-resizer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name               Apple Music - Artwork Changer
+// @name               Apple Music - Album Artwork Resizer
 // @version            1.0
-// @description        Change the image resolution and format for an artwork on Apple Music
+// @description        Change the image resolution and format for an album artwork on Apple Music
 // @author             MrBukLau
 // @homepage           https://github.com/MrBukLau/userscripts
 // @icon               https://music.apple.com/assets/favicon/favicon-180-c132a95549a91ae6983a4914da3e1c44.png
@@ -10,10 +10,10 @@
 // @run-at             document-start
 // ==/UserScript==
 
-if (location.href.match("1000x1000bb.webp")) {
-    location.href = location.href.replace("1000x1000bb.webp", "2000x2000.jpeg");
-}
-
 if (location.href.match("600x600bb.webp")) {
     location.href = location.href.replace("600x600bb.webp", "2000x2000.jpeg");
+}
+
+if (location.href.match("1000x1000bb.webp")) {
+    location.href = location.href.replace("1000x1000bb.webp", "2000x2000.jpeg");
 }
