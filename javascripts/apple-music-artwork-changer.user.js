e4327b5aa3198bf847293233c671c0587436cefa
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-12-09 18:40:10 -0800

    Update apple-music-artwork-changer.user.js

--- javascripts/apple-music-artwork-changer.user.js
+++ javascripts/apple-music-artwork-changer.user.js
@@ -2,10 +2,10 @@
 // @name               Apple Music - Artwork Changer
 // @version            1.0
 // @description        Change the image resolution and format for an artwork on Apple Music
+// @author             MrBukLau
+// @homepage           https://github.com/MrBukLau/userscripts
 // @icon               https://music.apple.com/assets/favicon/favicon-180-c132a95549a91ae6983a4914da3e1c44.png
 // @include            *mzstatic.com/image/thumb*
-// @homepage           https://github.com/MrBukLau/userscripts
-// @author             MrBukLau
 // @grant              none
 // @run-at             document-start
 // ==/UserScript==
