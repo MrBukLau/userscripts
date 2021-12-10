e00b589e475ea84221739679c6a4ab4c119db91d
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-12-09 18:41:55 -0800

    Update smart-app-banners-remover-for-ios.user.js

--- javascripts/smart-app-banners-remover-for-ios.user.js
+++ javascripts/smart-app-banners-remover-for-ios.user.js
@@ -2,10 +2,10 @@
 // @name               Smart App Banners Remover for iOS
 // @version            1.0
 // @description        Remove Smart App Banners from mobile websites on Safari for iOS
+// @author             MrBukLau
+// @homepage           https://github.com/MrBukLau/userscripts
 // @icon               https://developer.apple.com/favicon.ico
 // @match              *://*/*
-// @homepage           https://github.com/MrBukLau/userscripts
-// @author             MrBukLau
 // @grant              none
 // @run-at             document-start
 // ==/UserScript==
