6ff52b22a80d086985a8531c554c8bdb0cf7a6bc
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-12-09 18:40:53 -0800

    Update ifunny-pop-up-remover-for-ios.user.js

--- javascripts/ifunny-pop-up-remover-for-ios.user.js
+++ javascripts/ifunny-pop-up-remover-for-ios.user.js
@@ -2,10 +2,10 @@
 // @name               iFunny - Pop-up Remover for iOS
 // @version            1.0
 // @description        Remove pop-ups and banners from iFunny on Safari for iOS
+// @author             MrBukLau
+// @homepage           https://github.com/MrBukLau/userscripts
 // @icon               https://ifunny.co/favicon.ico
 // @match              https://ifunny.co/*
-// @homepage           https://github.com/MrBukLau/userscripts
-// @author             MrBukLau
 // @grant              none
 // @run-at             document-start
 // ==/UserScript==
