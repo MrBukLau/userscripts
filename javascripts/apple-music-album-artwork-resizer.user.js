a98caf12815f4d4b07eb40cc162a5e205a9c59f1
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-12-14 23:15:29 -0800

    Update apple-music-album-artwork-resizer.user.js

--- javascripts/apple-music-album-artwork-resizer.user.js
+++ javascripts/apple-music-album-artwork-resizer.user.js
@@ -4,6 +4,8 @@
 // @description        Change the image resolution and format for album artworks on Apple Music
 // @author             MrBukLau
 // @homepage           https://github.com/MrBukLau/userscripts
+// @downloadURL        https://github.com/MrBukLau/userscripts/raw/master/javascripts/apple-music-album-artwork-resizer.user.js
+// @updateURL          https://github.com/MrBukLau/userscripts/raw/master/javascripts/apple-music-album-artwork-resizer.user.js
 // @icon               https://music.apple.com/assets/favicon/favicon-180-c132a95549a91ae6983a4914da3e1c44.png
 // @include            *mzstatic.com/image/thumb*
 // @grant              none
