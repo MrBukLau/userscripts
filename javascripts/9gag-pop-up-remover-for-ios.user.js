eb37ec3dbb2ae60a2bf3071120b511334b45d724
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-12-14 23:14:20 -0800

    Update 9gag-pop-up-remover-for-ios.user.js

--- javascripts/9gag-pop-up-remover-for-ios.user.js
+++ javascripts/9gag-pop-up-remover-for-ios.user.js
@@ -4,6 +4,8 @@
 // @description        Remove pop-ups and banners from 9GAG on Safari for iOS
 // @author             MrBukLau
 // @homepage           https://github.com/MrBukLau/userscripts
+// @downloadURL        https://github.com/MrBukLau/userscripts/raw/master/javascripts/9gag-pop-up-remover-for-ios.user.js
+// @updateURL          https://github.com/MrBukLau/userscripts/raw/master/javascripts/9gag-pop-up-remover-for-ios.user.js
 // @icon               https://9gag.com/favicon.ico
 // @match              https://9gag.com/*
 // @grant              none
