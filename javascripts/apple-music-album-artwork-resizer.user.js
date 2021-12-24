2f33598b5e1d22e4b3fd09047c07af063838f997
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-12-24 11:55:18 -0800

    Update apple-music-album-artwork-resizer.user.js

--- javascripts/apple-music-album-artwork-resizer.user.js
+++ javascripts/apple-music-album-artwork-resizer.user.js
@@ -9,13 +9,17 @@
 // @downloadURL        https://github.com/MrBukLau/userscripts/raw/master/javascripts/apple-music-album-artwork-resizer.user.js
 // @updateURL          https://github.com/MrBukLau/userscripts/raw/master/javascripts/apple-music-album-artwork-resizer.user.js
 // @icon               https://music.apple.com/assets/favicon/favicon-180-c132a95549a91ae6983a4914da3e1c44.png
-// @include            *mzstatic.com/image/thumb*
+// @match              *://*mzstatic.com/image/thumb*
 // @grant              none
 // @run-at             document-start
 // ==/UserScript==
 
 if (location.href.match("600x600bb.webp")) {
     location.href = location.href.replace("600x600bb.webp", "2000x2000.jpeg");
+}
+
+if (location.href.match("600x600bf.webp")) {
+    location.href = location.href.replace("600x600bf.webp", "2000x2000.jpeg");
 }
 
 if (location.href.match("1000x1000bb.webp")) {
