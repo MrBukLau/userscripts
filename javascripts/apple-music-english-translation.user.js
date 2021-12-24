baafb1330003b5725b25428e8b0617de70850329
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2021-12-24 11:55:46 -0800

    Update apple-music-english-translation.user.js

--- javascripts/apple-music-english-translation.user.js
+++ javascripts/apple-music-english-translation.user.js
@@ -9,7 +9,7 @@
 // @downloadURL        https://github.com/MrBukLau/userscripts/raw/master/javascripts/apple-music-english-translation.user.js
 // @updateURL          https://github.com/MrBukLau/userscripts/raw/master/javascripts/apple-music-english-translation.user.js
 // @icon               https://music.apple.com/assets/favicon/favicon-180-c132a95549a91ae6983a4914da3e1c44.png
-// @include            *music.apple.com*
+// @match              *://music.apple.com/*
 // @grant              none
 // @run-at             document-start
 // ==/UserScript==
