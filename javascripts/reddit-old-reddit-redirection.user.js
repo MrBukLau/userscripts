ae31ad74f1b8d38dd39a70dbfb31c02d69954a36
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2022-01-16 21:24:31 -0800

    Update reddit-old-reddit-redirection.user.js

--- javascripts/reddit-old-reddit-redirection.user.js
+++ javascripts/reddit-old-reddit-redirection.user.js
@@ -2,13 +2,14 @@
 // @name               Reddit - Old Reddit Redirection
 // @namespace          https://github.com/MrBukLau
 // @version            1.0
-// @description        Avoid Reddit's redesign by redirecting Reddit links to Old Reddit
+// @description        Redirect Reddit links to Old Reddit
 // @author             MrBukLau
 // @homepageURL        https://github.com/MrBukLau/userscripts
 // @supportURL         https://github.com/MrBukLau/userscripts/issues
 // @downloadURL        https://github.com/MrBukLau/userscripts/raw/master/javascripts/reddit-old-reddit-redirection.user.js
 // @updateURL          https://github.com/MrBukLau/userscripts/raw/master/javascripts/reddit-old-reddit-redirection.user.js
 // @icon               https://old.reddit.com/favicon.ico
+// @match              *://np.reddit.com/*
 // @match              *://www.reddit.com/*
 // @exclude-match      *://www.reddit.com/poll/*
 // @grant              none
