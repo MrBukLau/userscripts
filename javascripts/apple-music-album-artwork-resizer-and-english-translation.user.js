59be0e0b24c3beb65a63882d5b68d5af2c4ce50a
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2022-01-29 19:28:17 -0800

    Create apple-music-album-artwork-resizer-and-english-translation.user.js

---
+++ javascripts/apple-music-album-artwork-resizer-and-english-translation.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name               Apple Music - Album Artwork Resizer and English Translation
+// @namespace          https://github.com/MrBukLau
+// @version            1.0
+// @description        Change the image format/resolution for album artworks on Apple Music and translate the words on Apple Music to English
+// @author             MrBukLau
+// @homepageURL        https://github.com/MrBukLau/userscripts
+// @supportURL         https://github.com/MrBukLau/userscripts/issues
+// @downloadURL        
+// @updateURL          
+// @icon               https://music.apple.com/assets/favicon/favicon-180-c132a95549a91ae6983a4914da3e1c44.png
+// @match              *://*mzstatic.com/image/thumb*
+// @match              *://music.apple.com/*
+// @grant              none
+// @run-at             document-start
+// ==/UserScript==
